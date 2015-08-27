35ca86331fadbbd8f6176dabbf9cf97a6f5f9101
Robert Quattlebaum <darco@deepdarc.com>
2015-08-27 19:15:31 +0000

    nginx

--- Dockerfile
+++ Dockerfile
@@ -1,31 +1,7 @@
-FROM debian:wheezy
+FROM nginx
 
-ENV DEBIAN_FRONTEND noninteractive
+EXPOSE 80/tcp 443/tcp
 
-ADD squid-openssl.patch squid-openssl.patch
-
-RUN apt-get -y update \
-	&& DEBIAN_FRONTEND=noninteractive \
-		apt-get -y install devscripts build-essential fakeroot libssl-dev \
-	&& ( cat /etc/apt/sources.list | sed 's/^deb /deb-src /' > temp ) \
-	&& cat temp >> /etc/apt/sources.list \
-	&& DEBIAN_FRONTEND=noninteractive apt-get -y update \
-	&& DEBIAN_FRONTEND=noninteractive apt-get -y source squid \
-	&& DEBIAN_FRONTEND=noninteractive apt-get -y build-dep squid \
-	&& cd squid*/ \
-	&& ( patch -l -p1 < /squid-openssl.patch ) \
-	&& DEBIAN_FRONTEND=noninteractive dpkg-buildpackage \
-	&& cd .. \
-	&& ( dpkg -i *.deb ; DEBIAN_FRONTEND=noninteractive apt-get -y -f install ) \
-	&& DEBIAN_FRONTEND=noninteractive apt-get -y purge devscripts build-essential fakeroot libssl-dev \
-	&& DEBIAN_FRONTEND=noninteractive apt-get -y autoremove \
-	&& DEBIAN_FRONTEND=noninteractive apt-get -y clean \
-	&& rm -fr squid* \
-	&& true
-
-EXPOSE 80/tcp 443/tcp 8080/tcp 8081/tcp
-
-ADD squid.conf /etc/squid/squid.conf
 ADD self-signed-cert.cfg self-signed-cert.cfg
 ADD start.sh /start.sh
 RUN chmod +x /start.sh
@@ -34,6 +10,8 @@ ADD reverse-proxy.conf /etc/reverse-proxy/reverse-proxy.conf
 
 # For debugging
 #RUN apt-get -y install procps vim tmux man less net-tools
+
+RUN ln -sf /dev/stdout /var/log/nginx/error.log
 
 VOLUME ["/etc/ssl/certs", "/etc/ssl/private", "/etc/reverse-proxy"]
 
--- start.sh
+++ start.sh
@@ -1,28 +1,64 @@
 #!/bin/bash
 
 VHOSTNUMBER=0
+#set -x
 
-PROXY_CLIENT_CA=/etc/ssl/certs/proxy-client-ca.pem
+[ "x${PROXY_CLIENT_CA}" = "x" ] && export PROXY_CLIENT_CA=/etc/ssl/certs/proxy-client-ca.pem
 
-set -x
+NGINX_CONFIG_FILE=/etc/nginx/conf.d/default.conf
+
+update-ca-certificates
+	
+export SSL_CIPHER="ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-AES256-GCM-SHA384:DHE-RSA-AES128-GCM-SHA256:DHE-DSS-AES128-GCM-SHA256:kEDH+AESGCM:ECDHE-RSA-AES128-SHA256:ECDHE-ECDSA-AES128-SHA256:ECDHE-RSA-AES128-SHA:ECDHE-ECDSA-AES128-SHA:ECDHE-RSA-AES256-SHA384:ECDHE-ECDSA-AES256-SHA384:ECDHE-RSA-AES256-SHA:ECDHE-ECDSA-AES256-SHA:DHE-RSA-AES128-SHA256:DHE-RSA-AES128-SHA:DHE-DSS-AES128-SHA256:DHE-RSA-AES256-SHA256:DHE-DSS-AES256-SHA:DHE-RSA-AES256-SHA:!aNULL:!eNULL:!EXPORT:!DES:!RC4:!3DES:!MD5:!PSK"
+export HOST_SSL_CERT=/etc/ssl/certs/host.crt.pem
+export HOST_SSL_KEY=/etc/ssl/private/host.key.pem
+export HTTPS_PORT=
+export HTTP_PORT=
+
+# FD 3 is the configuration file
+
+die () {
+	echo "FATAL: $*"
+	exit 1
+}
+
+add-server-port-stuff() {
+	if [ "x$HTTPS_PORT" != "x" ]
+	then
+		echo "	listen $HTTPS_PORT ssl;"
+		echo "	ssl on;"
+        echo "	ssl_certificate ${HOST_SSL_CERT};"
+        echo "	ssl_certificate_key ${HOST_SSL_KEY};"
+        echo "	ssl_session_timeout  5m;"
+        #echo "	ssl_protocols        SSLv3 TLSv1;"
+        echo "	ssl_ciphers          $SSL_CIPHER;"
+        echo "	ssl_prefer_server_ciphers   on;"
+		echo "  ssl_verify_depth 2;"
+		
+		[ -f "${PROXY_CLIENT_CA}" ] && {
+			echo "  ssl_client_certificate ${PROXY_CLIENT_CA};"
+			echo "  ssl_verify_client on;"
+		}
+	else
+		echo "	listen $HTTP_PORT;"
+	fi
+}
 
 add-http-vhost () {
-	local name=vhost-$((VHOSTNUMBER++))
 	local address=$1
 	local port=$2
 	shift
 	shift
 	
 	(
-		echo "############ HTTP -> $*"
-		echo "cache_peer ${address} parent ${port} 0 no-query originserver name=${name}"
-		echo "cache_peer_domain ${name} dstdomain $*"
-		echo "acl ${name}-acl dstdomain $*"
-		echo "http_access allow ${name}-acl"
-		echo "cache_peer_access ${name} allow ${name}-acl"
-		echo "#cache_peer_access ${name} deny all"
-		echo ""
-	) >> /etc/squid/squid-custom.conf
+		echo "server {"
+		add-server-port-stuff
+		echo "	server_name $*;"
+		echo "	location / {"
+		echo "		proxy_pass http://$address:$port;"
+		echo "	}"
+		echo "}"
+	) 1>&3
 }
 
 add-https-vhost () {
@@ -31,58 +67,65 @@ add-https-vhost () {
 	local port=$2
 	shift
 	shift
-
+	
 	(
-		echo "############ HTTPS -> $*"
-		echo "cache_peer ${address} parent ${port} 0 no-query originserver ssl sslflags=DONT_VERIFY_PEER name=${name}"
-		echo "cache_peer_domain ${name} dstdomain $*"
-		echo "acl ${name}-acl dstdomain $*"
-		echo "http_access allow ${name}-acl"
-		echo "cache_peer_access ${name} allow ${name}-acl"
-		echo "#cache_peer_access ${name} deny all"
-		echo ""
-	) >> /etc/squid/squid-custom.conf
+		echo "server {"
+		add-server-port-stuff
+		echo "	server_name $*;"
+		echo "	location / {"
+		echo "		proxy_pass https://$address:$port;"
+		echo "	}"
+		echo "}"
+	) 1>&3
 }
 
 http-listen () {
 	local port=$1
-	(
-		echo "http_port ${port} accel vhost"
-	) >> /etc/squid/squid-custom.conf
+	export HTTP_PORT=$1
+	echo "# http-listen" 1>&3
+	
+	[ "x$HTTPS_PORT" != "x" ] && require-https
 }
 
 https-listen () {
-	local port=$1
-	local cipher="ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-AES256-GCM-SHA384:DHE-RSA-AES128-GCM-SHA256:DHE-DSS-AES128-GCM-SHA256:kEDH+AESGCM:ECDHE-RSA-AES128-SHA256:ECDHE-ECDSA-AES128-SHA256:ECDHE-RSA-AES128-SHA:ECDHE-ECDSA-AES128-SHA:ECDHE-RSA-AES256-SHA384:ECDHE-ECDSA-AES256-SHA384:ECDHE-RSA-AES256-SHA:ECDHE-ECDSA-AES256-SHA:DHE-RSA-AES128-SHA256:DHE-RSA-AES128-SHA:DHE-DSS-AES128-SHA256:DHE-RSA-AES256-SHA256:DHE-DSS-AES256-SHA:DHE-RSA-AES256-SHA:!aNULL:!eNULL:!EXPORT:!DES:!RC4:!3DES:!MD5:!PSK"
-	local ssl_options="NO_SSLv3,NO_TLSv1,SINGLE_DH_USE"
+	export HTTPS_PORT=$1
 	local dhparams=/etc/ssl/certs/dhparam.pem
 
 	[ -f "$dhparams" ] || openssl dhparam -out "$dhparams" 2048
 
+	[ -f /etc/ssl/certs/host.crt.pem ] || {
+		openssl req -x509 -nodes -days 3650 -newkey rsa:2048 -keyout /etc/ssl/private/host.key.pem -out /etc/ssl/certs/host.crt.pem -config self-signed-cert.cfg
+	}
+	echo "# https-listen" 1>&3
+
+	[ "x$HTTP_PORT" != "x" ] && require-https
+}
+
+require-https () {
 	(
-		echo -n "https_port ${port} accel vhost cert=/etc/ssl/certs/host.crt.pem key=/etc/ssl/private/host.key.pem options=$ssl_options dhparams=$dhparams cipher=$cipher capath=/etc/ssl/certs"
-		[ -f "${PROXY_CLIENT_CA}" ] && echo "  clientca=${PROXY_CLIENT_CA}"
-		echo ""
-	) >> /etc/squid/squid-custom.conf
+		echo " server {"
+		echo "	listen ${HTTP_PORT};"
+		echo "	server_name "'~.*'";"
+		echo '	rewrite ^ https://$host$request_uri? permanent;'
+		echo "}"
+	) 1>&3
 }
 
 ###################################################
 
-[ -f /etc/ssl/certs/host.crt.pem ] || {
-	openssl req -x509 -nodes -days 3650 -newkey rsa:2048 -keyout /etc/ssl/private/host.key.pem -out /etc/ssl/certs/host.crt.pem -config self-signed-cert.cfg
-}
 
-if [ -f /etc/squid/squid-prefix.conf ]
-then cat /etc/squid/squid-prefix.conf /etc/squid/squid.conf > /etc/squid/squid-custom.conf
-else cat /etc/squid/squid.conf > /etc/squid/squid-custom.conf
+if [ -f /etc/squid3/squid-prefix.conf ]
+then cat /etc/squid3/squid-prefix.conf /etc/squid3/squid.conf > "${SQUID_CONFIG_FILE}"
+else cat /etc/squid3/squid.conf > "${SQUID_CONFIG_FILE}"
 fi
 
-cat /etc/squid/squid.conf > /etc/squid/squid-custom.conf
+cat /etc/squid3/squid.conf > "${SQUID_CONFIG_FILE}"
 
 ###################################################
 
-. /etc/reverse-proxy/reverse-proxy.conf
+( . /etc/reverse-proxy/reverse-proxy.conf ) 3> "${NGINX_CONFIG_FILE}" || die Creation of configuration file failed
 
 ###################################################
 
-exec /usr/sbin/squid -d 10 -N -f /etc/squid/squid-custom.conf
+exec nginx -g "daemon off;"
+
